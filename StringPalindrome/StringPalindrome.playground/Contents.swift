6c97c7300efb7bfbfd9345dec2084881fa6d7cc5
Anilkumar-ms <54616693+Anilkumar-ms@users.noreply.github.com>
2023-01-10 11:20:16 +0530

    Adding Third approach with better optimisation

--- StringPalindrome/StringPalindrome.playground/Contents.swift
+++ StringPalindrome/StringPalindrome.playground/Contents.swift
@@ -1,41 +1,59 @@
-import UIKit
+func checkPalindromeForInputString(_ inputString: String) -> Bool {
+    guard inputString.count > 1 else { return true }
 
-func checkPalindrome1(inputString: String) -> Bool {
     let stringLength = inputString.count
-        var position = 0
+    var position = 0
         
-        while position < stringLength / 2 {
-            let startIndex = inputString.index(inputString.startIndex, offsetBy: position)
-            let endIndex = inputString.index(inputString.endIndex, offsetBy: -position - 1)
+    while position < stringLength / 2 {
+    let startIndex = inputString.index(inputString.startIndex, offsetBy: position)
+    let endIndex = inputString.index(inputString.endIndex, offsetBy: -position - 1)
             
-            if inputString[startIndex] == inputString[endIndex] {
-                position += 1
-            } else {
-                return false
-            }
+    if inputString[startIndex] == inputString[endIndex] {
+        position += 1
+    } else {
+        return false
         }
-        
-        return true
+    }
+    return true
 }
 
-func checkPalindrome2(inputString: String) -> Bool {
+func checkPalindromeForInputString1(_ inputString: String) -> Bool {
     guard inputString.count > 1 else { return true }
     
     if inputString.first == inputString.last {
         let range = inputString.index(after: inputString.startIndex)..<inputString.index(before: inputString.endIndex)
         let substring = inputString[range]
-        print(substring)
-       return checkPalindrome2(inputString: String(substring))
+       return checkPalindromeForInputString(String(substring))
     } else {
         return false
     }
 
 }
 
+func checkPalindromeForInputString2(_ inputString: String) -> Bool {
+    guard inputString.count > 1 else { return true }
+    let inputStringArray = Array(inputString)
+    var i = 0, j = inputString.count-1
+    while (i < j) {
+        if inputStringArray[i] ==  inputStringArray[j] {
+            i += 1
+            j -= 1
+        } else {
+            return false
+        }
+    }
+    if i > j {
+        return false
+    }
+    return true
+}
+print(checkPalindromeForInputString("aa1baa"))
+print(checkPalindromeForInputString("aa1b1aa"))
+print(checkPalindromeForInputString("aa1b1aa"))
+print(checkPalindromeForInputString1("aa1baa"))
+print(checkPalindromeForInputString1("aa1b1aa"))
+print(checkPalindromeForInputString1("aa1b1aa"))
+print(checkPalindromeForInputString2("aa1baa"))
+print(checkPalindromeForInputString2("aa1b1aa"))
+print(checkPalindromeForInputString2("aa1b1aa"))
 
-print(checkPalindrome1(inputString: "aabaa"))
-print(checkPalindrome1(inputString: "aa1baa"))
-print(checkPalindrome1(inputString: "aa1b1aa"))
-print(checkPalindrome2(inputString: "aabaa"))
-print(checkPalindrome2(inputString: "aa1baa"))
-print(checkPalindrome2(inputString: "aa1b1aa"))
